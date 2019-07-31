966fd7531764b6835d5f47f87422ab14bb526164
Tate Shepard <tateshepard@yahoo.com>
2019-07-31 23:09:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,13 +9,11 @@
 #end
 
 def square_array(array)
-  counter = 0
-  array = [1,2,3,]
- 
- 
-  while counter< array.length do
-  array << array[counter]**2
-  counter += 1
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
   end
-  array
+  squared
 end
