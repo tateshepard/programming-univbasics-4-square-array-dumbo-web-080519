8a3c25d7b7175be8b218b69c0bc367be2bbf1e8d
Tate Shepard <tateshepard@yahoo.com>
2019-07-31 22:19:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,11 +8,12 @@
 #  return your new array at end of method
 #end
 
-
-counter = 0
-array = [1,2,3,]
+def square_array(array)
+  counter = 0
+  array = [1,2,3,]
  
-while array[counter] do
+  while array[counter] do
   puts array[counter]**2
   counter += 1
+  end
 end
