aafa6d15884a9a4fe0aa68e6ac7be912c0b09921
Tate Shepard <tateshepard@yahoo.com>
2019-07-31 21:35:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,25 @@
+#def square_array(array)
+ #create new array
+  #create counter
+  #start while loop that goes on while counter is less than length of array
+   # inside loop
+    #  shovel the squared element into new array you created above
+     # increment counter
+#  return your new array at end of method
+#end
+
+counter = 0
+numbers = [1,2,3]
+ 
+while numbers[counter] do
+  puts numbers[counter]
+  counter *= 1
+end
+
 def square_array(array)
-  # your code here
+ array = []
+ counter = 0 
+ while array[counter] do
+    puts array[counter]
+    counter += |1|
 end
