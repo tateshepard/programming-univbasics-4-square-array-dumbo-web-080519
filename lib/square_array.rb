41d400154a32c349fb3077fbabdf3209496a4e02
Tate Shepard <tateshepard@yahoo.com>
2019-07-31 22:18:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,12 +9,10 @@
 #end
 
 
-def square_array(array)
-  counter = 0
-
-  while array[counter] do
-    puts array[counter]
-    counter += 1
-  end
-end
+counter = 0
+array = [1,2,3,]
  
+while array[counter] do
+  puts array[counter]**2
+  counter += 1
+end
